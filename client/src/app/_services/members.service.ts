d1163c7a63d8bbb205791cb438e6273a572b8ce4
angel <fsguoangel@hotmail.com>
2021-10-29 08:18:45 +0800

    End of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { AccountService } from './account.service';
@@ -9,15 +11,31 @@ import { AccountService } from './account.service';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
   constructor(private http: HttpClient, private accountService: AccountService) {
 
   }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(response => {
+      this.members = response;
+      return response;
+    }))
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/'+username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+    }))
   }
 }
